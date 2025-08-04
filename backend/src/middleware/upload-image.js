875b01aa13d273c0045f50bdf29af59370a9d895
bee <beekingword@gmail.com>
2025-08-04 22:35:33 +0700

    Bee

---
+++ backend/src/middleware/upload-image.js
@@ -0,0 +1,36 @@
+const multer = require('multer');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require('../cloudinary/config');
+
+const fileFilter = (req, file, cb) => {
+  
+  if (file.mimetype.startsWith('image/')) {
+    cb(null, true); 
+  } else {
+    cb(new Error('ກະລຸນາອັບໂຫຼດສະເພາະໄຟລ໌ຮູບເທົ່ານັ້ນ!'), false);
+  }
+};
+
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: 
+   {
+      folder: 'M-moneyX',
+      allowed_formats: ["jpg", "jpeg", "png"],
+      transformation: [{ width: 800, height: 600, crop: 'limit' }], 
+      public_id: `M-moneyX_${req.user.id}_${Date.now()}`, 
+    }
+  },
+);
+
+const upload = multer({
+  storage: storage, 
+  limits: {
+    fileSize: 5 * 1024 * 1024, 
+  },
+  fileFilter: fileFilter, 
+});
+
+
+module.exports = upload;
