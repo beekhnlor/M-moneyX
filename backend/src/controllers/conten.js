bed9c4814a437bc992b5593bd4e9bc139c6d35ff
bee <beekingword@gmail.com>
2025-08-05 22:28:56 +0700

    Bee 5/8/2025

--- backend/src/controllers/conten.js
+++ backend/src/controllers/conten.js
@@ -2,10 +2,14 @@ const connected = require("../connectdb/connecting");
 const queries = require("../queries/query");
 
 const content = async (req, res) => {
-  const { section_id, title, subtitle, description} = req.body;
-  const images = req.files?.path || null;
+  const { section_id, title, subtitle, description } = req.body;
 
   try {
+
+    const imageUrls = req.files.map(file => file.path); 
+
+    const images = imageUrls.join(',');
+
     const [Contented] = await connected.query(queries.content, [
       section_id,
       title,
@@ -16,15 +20,35 @@ const content = async (req, res) => {
       new Date(),
     ]);
 
-    return res
-      .status(200)
-      .json({ message: "Content Insert Success ", Contented });
+    return res.status(200).json({
+      message: "Content Insert Success",
+      images: imageUrls,
+      Contented,
+    });
   } catch (err) {
     console.log(err);
     return res.status(500).json({ message: "Internal Server Error" });
   }
 };
 
+const getContents = async (req, res) => {
+  try {
+    const [rows] = await connected.query("SELECT * FROM tb_content");
+
+    const result = rows.map(content => ({
+      ...content,
+      images: content.images ? content.images.split(',') : [],
+    }));
+
+    return res.status(200).json({message:"Select db Success",result});
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "Fetch Error" });
+  }
+};
+
+
 module.exports = {
   content,
+  getContents
 };
--- backend/src/routes/content.js
+++ backend/src/routes/content.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const { content  } = require('../controllers/conten')
+const { content,getContents  } = require('../controllers/conten')
 const upload = require('../middleware/upload-image')
 const { authCheck } = require('../middleware/auth')
 
 router.post('/content-upload-images',authCheck,upload.array('image',10),content)
+router.get('/getcontent',getContents)
 
 module.exports = router
